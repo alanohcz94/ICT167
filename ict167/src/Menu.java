466d546a9ebfb1ccfcecae360275051a342dfdad
AoCz <64344100+alanohcz94@users.noreply.github.com>
2021-05-13 00:24:51 +0800

    version2
    Updated the searchCus(boolean), completed the denoCal function

--- ict167/src/Menu.java
+++ ict167/src/Menu.java
@@ -7,14 +7,19 @@
  *   Incomplete functions to be continued: denoCal, writeToCSV,
  *   Date:   11/5/2021
  *
+ *   Description: Finishing and complete denoCal function and ensure all code is runnable
+ *   Changes: Updated the searchCus(boolean), completed the denoCal
+ *   Version:   2
+ *   Date 12/5/2021
+ *
  */
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-
 public class Menu {
     List<Customer> customerList = new ArrayList<>();
 
@@ -48,6 +53,9 @@ public class Menu {
 
     // search for the customer if they exist or not
     private boolean searchCus(String name, int amount){
+        if(amount%5 != 0) {
+            return true;
+        }
         for(Customer c : customerList){
             String cusName = c.getName();
             int cusAmount = c.getCoins();
@@ -72,9 +80,45 @@ public class Menu {
 
     //incomplete need to work on tomorrow
     private String denoCal(int amount, int choice){
-
-
-        return "Nothing for now";
+        int cents50=0, cents20=0, cents10=0, cents5=0;
+        while(amount != 0){
+            while(amount >= 50){
+                cents50++;
+                amount -= 50;
+            }
+            while(amount >= 20){
+                cents20++;
+                amount -= 20;
+            }
+            while(amount >= 10){
+                cents10++;
+                amount -= 10;
+            }
+            while(amount >= 5){
+                cents5++;
+                amount -= 5;
+            }
+        }
+        if(choice == 1){
+            String line50 = "50 Cents: "+cents50 ,
+                    line20= "20 Cents: "+cents20 ,
+                    line10= "10 Cents: "+cents10 ,
+                    line5= "5 Cents: "+cents5 ;
+            if(cents50 != 0){
+                System.out.println(line50);
+            }
+            if(cents20 != 0){
+                System.out.println(line20);
+            }
+            if(cents10 != 0){
+                System.out.println(line10);
+            }
+            if(cents5 != 0){
+                System.out.println(line5);
+            }
+            return "";
+        }   //choice is two
+        return cents50+","+cents20+","+cents10+","+cents5;
     }
 
     //incomplete need to work on tomorrow
@@ -90,11 +134,14 @@ public class Menu {
                     String name = s.next();
                     String result = searchCus(name);
                     System.out.println(result);
+                    break;
                 case 2:
                     //write to csv
                     System.exit(1);
+                    break;
                 default:
                     System.out.println("Incorrect input of choice please try again");
+                    break;
             }
         }
     }
